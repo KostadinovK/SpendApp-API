288919be533defac1961e3cdf25356175b4dc13f
KostadinovK <kostadinovk2001@gmail.com>
2019-12-30 20:31:13 +0200

    Added corsOptions with multiple origin

--- app.js
+++ app.js
@@ -14,7 +14,20 @@ const incomesRouter = require('./routes/incomes');
 
 const app = express();
 
-app.use(cors({credentials: true, origin: 'http://127.0.0.1:5501'}));
+let whitelist = ['http://127.0.0.1:5501', 'https://kostadinovk.github.io'];
+
+let corsOptions = {
+    credentials: true,
+    origin: function (origin, callback) {
+        if (whitelist.indexOf(origin) !== -1) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    }
+};
+
+app.use(cors(corsOptions));
 
 app.use(cookieParser('secret'))
 app.use(express.json());
